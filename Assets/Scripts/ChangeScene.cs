b9d86dfd7c33c89f21f7e52e80cfa1aacebb69d3
Allison Kim <k39728@outlook.com>
2020-07-11 21:06:56 +0900

    Working on main scene
    I hate Unity. What the heck. Just destroyed everything I've been working on for a whole freaking month

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void NextScene(string next_scene)
+    {
+        SceneManager.LoadScene(next_scene);
+    }
+}
