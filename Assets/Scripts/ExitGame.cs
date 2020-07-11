7bac388bdafedd0445f7ec9049736d16dbec6633
Allison Kim <k39728@outlook.com>
2020-07-12 00:52:36 +0900

    Added useless scenes

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("bye");
+        }
+    }
+}
