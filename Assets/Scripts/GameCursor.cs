edfe9a8c4b9ac8876841e0696b9e2cb917491981
Allison Kim <k39728@outlook.com>
2020-07-13 18:04:28 +0900

    Added paused-box and cursor

---
+++ Assets/Scripts/GameCursor.cs
@@ -0,0 +1,25 @@
+﻿/*
+
+   .-------.                             .--.    .-------.     .--.            .--.     .--.        
+   |       |--.--.--------.-----.-----.--|  |    |_     _|--.--|  |_.-----.----|__|---.-|  |-----.
+   |   -   |_   _|        |  _  |     |  _  |      |   | |  |  |   _|  _  |   _|  |  _  |  |__ --|
+   |_______|__.__|__|__|__|_____|__|__|_____|      |___| |_____|____|_____|__| |__|___._|__|_____|
+   © 2019 OXMOND / www.oxmond.com 
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCursor : MonoBehaviour
+{
+    public Texture2D cursorArrow;
+
+    void Start()
+    {
+        // Cursor.visible = false;
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
+
